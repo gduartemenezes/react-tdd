4e3a7da0dd3f601d443c645106518fd7cac07eb3
Gustavo Duarte <gduartemenezes@gmail.com>
2023-08-29 08:32:28 -0300

    refactor: move protocols folder to data folder

--- src/domain/usecases/data/usecases/authentication/remote-authentication.spec.ts
+++ src/domain/usecases/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,4 +1,4 @@
-import { HttpPostClient } from './protocols/http/http-post-client'
+import { HttpPostClient } from '../../protocols/http/http-post-client'
 import { RemoteAuthenctication } from './remote-authentication'
 
 describe('RemoteAuthentication', () => {
