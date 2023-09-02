26556ea4c6a535545a7b4930fb6d6183f7739d31
Gustavo Duarte <gduartemenezes@gmail.com>
2023-09-02 12:51:16 -0300

    refactor: move form-status to a separated file

---
+++ src/presentation/components/form-status/form-status.tsx
@@ -0,0 +1,12 @@
+
+import React, { memo } from 'react'
+import Styles from './form-status-styles.scss'
+import Spinner from '../spinner/spinner'
+const FormStatus: React.FC = () => {
+  return <div className={Styles.errorWrap}>
+    <span className={Styles.error}>Errou</span>
+    <Spinner className={Styles.spinner} />
+  </div>
+}
+
+export default FormStatus
