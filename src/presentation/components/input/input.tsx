39cae82ed7623234511ab0bb22ce212106afaa1f
Gustavo Duarte <gduartemenezes@gmail.com>
2023-09-04 22:01:50 -0300

    feat: ensure Login calls validation with correct email

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -4,14 +4,21 @@ import Styles from './input-styles.scss'
 import Context from '@/presentation/contexts/form/form-context'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 const Input: React.FC<Props> = (props: Props) => {
-  const { errorState } = useContext(Context)
-  const error = errorState[props.name]
+  const { state, setState } = useContext(Context)
+  const error = state[`${props.name}Error`]
   const getStatus = (): string => {
     return '🔴'
   }
+
   const getTitle = (): string => { return error }
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    setState({
+      ...state,
+      [event.target.name]: event.target.value
+    })
+  }
   return <div className={Styles.inputWrap}>
-    <input autoComplete='off' {...props} />
+    <input autoComplete='off' {...props} data-testid={props.name} onChange={handleChange} />
     <span data-testid={`${props.name}-status`} title={getTitle()} className={Styles.status} >{getStatus()}</span>
   </div>
 }
--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,23 +1,28 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Styles from './login-styles.scss'
 import { Header, Footer, Input, FormStatus } from '@/presentation/components/'
 import Context from '@/presentation/contexts/form/form-context'
+import { Validation } from '@/presentation/protocols/validation'
 
-const Login: React.FC = () => {
+type Props = {
+  validation: Validation
+}
+const Login: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
-    isLoading: false
-
+    isLoading: false,
+    email: '',
+    emailError: 'Campo obrigatório',
+    passwordError: 'Campo obrigatório',
+    mainError: ''
   })
-  const [errorState, setErrorState] = useState({
-    email: 'Campo obrigatório',
-    password: 'Campo obrigatório',
-    main: ''
+  useEffect(() => {
+    validation.validate({ email: state.email })
+  }, [state.email])
 
-  })
   return (
     <div className={Styles.login}>
       <Header />
-      <Context.Provider value={{ state, errorState }}>
+      <Context.Provider value={{ state, setState }}>
         <form action="" className={Styles.form}>
           <h2>Login</h2>
           <Input type="email" name="email" placeholder='Digite seu email' id="" />
