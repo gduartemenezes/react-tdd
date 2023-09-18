262cd40cb52c56378c42b11aa3178e13f34ff5d6
Gustavo Duarte <gduartemenezes@gmail.com>
2023-09-18 14:01:32 -0300

    refactor: move sut to it's own method

--- src/validation/required-field-validation.spec.ts
+++ src/validation/required-field-validation.spec.ts
@@ -3,6 +3,8 @@ import { RequiredFieldValidation } from './required-field.validation'
 import { RequiredFieldError } from './errors'
 import { faker } from '@faker-js/faker'
 
+const makeSut = (): RequiredFieldValidation => new RequiredFieldValidation(faker.lorem.sentence())
+
 describe('', () => {
   afterEach(cleanup)
   beforeEach(() => {
@@ -10,14 +12,14 @@ describe('', () => {
   })
 
   test('Should return error if field is empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate('')
 
     expect(error).toEqual(new RequiredFieldError())
   })
 
   test('Should return falsy if field is not empty', () => {
-    const sut = new RequiredFieldValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.lorem.sentence())
     expect(error).toBeFalsy()
   })
