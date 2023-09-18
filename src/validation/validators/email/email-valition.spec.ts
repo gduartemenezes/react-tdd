0a0bcb8ec9227d827056270ced1d700731960654
Gustavo Duarte <gduartemenezes@gmail.com>
2023-09-18 20:43:27 -0300

    refactor: move sut to it's own method

--- src/validation/validators/email/email-valition.spec.ts
+++ src/validation/validators/email/email-valition.spec.ts
@@ -2,15 +2,17 @@ import { faker } from '@faker-js/faker'
 import { EmailValidation } from './email-validation'
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 
+const makeSut = (): EmailValidation => new EmailValidation('email')
+
 describe('EmailValidation', () => {
   test('Should return an error if email is invalid', () => {
-    const sut = new EmailValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.lorem.word())
     expect(error).toEqual(new InvalidFieldError('email'))
   })
 
   test('Should return falsy if email is valid', () => {
-    const sut = new EmailValidation('email')
+    const sut = makeSut()
     const error = sut.validate(faker.internet.email())
     expect(error).toBeFalsy()
   })
