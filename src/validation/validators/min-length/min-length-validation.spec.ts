f416b61868a51d41645da298dc0efbffb9f2bf9a
Gustavo Duarte <gduartemenezes@gmail.com>
2023-09-19 09:55:33 -0300

    refactor: move sut to it own file

--- src/validation/validators/min-length/min-length-validation.spec.ts
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -1,17 +1,20 @@
 import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
 import { FieldValidation } from '@/validation/protocols/field-validation'
 import { MinLengthValidation } from '@/validation/validators/min-length/min-length-validation'
+import { faker } from '@faker-js/faker'
+
+const makeSut = (): MinLengthValidation => new MinLengthValidation(faker.database.column(), 5)
 
 describe('MinLengthValidation', () => {
   test('Should return error if field length is less than specified', () => {
-    const sut = new MinLengthValidation('field', 5)
-    const error = sut.validate('123')
+    const sut = makeSut()
+    const error = sut.validate(faker.lorem.word({ length: { max: 4, min: 1 } }))
     expect(error).toEqual(new InvalidFieldError('field'))
   })
 
   test('Should falsy field length is bigger or equal than specified', () => {
-    const sut = new MinLengthValidation('field', 5)
-    const error = sut.validate('12345')
+    const sut = makeSut()
+    const error = sut.validate(faker.lorem.word({ length: { max: 10, min: 5 } }))
     expect(error).toBeFalsy()
   })
 })
