121bbb117c73e3c86a7524c79d33dbf00cbb6ade
Gustavo Duarte <gduartemenezes@gmail.com>
2023-09-19 09:32:45 -0300

    test: ensure MinLengthValidation returns error if field length is less than specified

---
+++ src/validation/validators/min-length/min-length-validation.spec.ts
@@ -0,0 +1,11 @@
+import { InvalidFieldError } from '@/validation/errors/invalid-field-error'
+import { FieldValidation } from '@/validation/protocols/field-validation'
+import { MinLengthValidation } from '@/validation/validators/min-length/min-length-validation'
+
+describe('MinLengthValidation', () => {
+  test('Should return error if field length is less than specified', () => {
+    const sut = new MinLengthValidation('field', 5)
+    const error = sut.validate('123')
+    expect(error).toEqual(new InvalidFieldError('field'))
+  })
+})
